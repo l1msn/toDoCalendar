0efe5b0fb7b305918f871239c4d81aba45982450
l1msn <Mydarlingg99>
2023-02-21 21:25:08 +0300

    Add first pages and start configure state manager - redux

---
+++ src/pages/Event.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Event: React.FC = (): JSX.Element => {
+  return (
+    <div>
+        Event
+    </div>
+  );
+};
+
+export default Event;
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+
+})
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch;
+
+export default store;
+export type { RootState, AppDispatch };
